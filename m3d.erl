daee0c379f2312608e9e92d2dbabea36c00f6d9d
Olivier Girondel <olivier@biniou.info>
2009-12-23 15:32:27 +0100

    New version

--- m3d.erl
+++ m3d.erl
@@ -2,29 +2,44 @@
 -author('olivier@biniou.info').
 
 %% API
--export([start/0]).
+-export([start/0, start/1]).
 
 %% Internal exports
 -export([m3d1/3]).
 
 %% Macros
--define(N,   10).
--define(MAX, lists:seq(0, ?N-1)).
+-define(N,     10).
+-define(MAX,   lists:seq(0, ?N-1)).
+-define(FNAME, "test.df3"). %% default filename
 
 
-%% oliv3: relou l'ordre x/y/z ca oblige a faire
-%% un lists:reverse on peut pas faire autrement ?
+-define(XMIN, -2.5).
+-define(XMAX, +1.5).
+-define(YMIN, -2.0).
+-define(YMAX, +2.0).
+-define(ZMIN, -2.0).
+-define(ZMAX, +2.0).
+
+
 start() ->
-    Refs = [m3d(X, Y, Z) || Z <- ?MAX,
-			    Y <- ?MAX,
-			    X <- ?MAX],
-    Res = collect(Refs),
-    io:format("Res= ~p~n", [Res]),
-    Res.
+    start(?FNAME).
+start(Filename) ->
+    %% open file for writing
+    case file:open(Filename, [write, binary]) of
+	{ok, Fd} ->
+	    main(Fd),
+	    file:close(Fd);
+	Error ->
+	    exit(Error)
+    end.
+
+
+main(Fd) ->
+    ok.
 
 
 m3d(X, Y, Z) ->
-    %% io:format("~p ~p ~p~n", [X, Y, Z]),
+    io:format("~p ~p ~p~n", [X, Y, Z]),
     Ref = make_ref(),
     spawn(?MODULE, m3d1, [self(), Ref, {X, Y, Z}]),
     Ref.
