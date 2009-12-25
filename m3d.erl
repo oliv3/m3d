e0f02123856cdc3f3e092bcc0c44ca3b7b2fb351
Olivier Girondel <olivier@biniou.info>
2009-12-25 02:53:50 +0100

    Spawn new calculations as soon as possible

--- m3d.erl
+++ m3d.erl
@@ -2,19 +2,35 @@
 -author('olivier@biniou.info').
 
 %% API
--export([start/0, start/1]).
+-export([s/0, start/0, start/1]).
+-export([point/0]).
 
 %% Internal exports
--export([m3d1/3]).
+-export([gen0/1, m3d/2]).
 
 %% Macros
--define(SIZE,    10). %% TODO 1000
--define(FNAME,   "test.df3"). %% default filename
--define(EXPVAL,  8).
--define(MAXITER, 16#ffff).
+-define(SIZE,     3).        %% TODO even more :p
+-define(FNAME,    "test.df3"). %% default filename
+-define(EXPVAL,   8).          %% 2.0 to ..., TODO test with 1/... or -...
+-define(CORES,    4).
 
--define(XMIN, -1.5).
--define(XMAX, +0.5).
+-define(MAXITERl, 16#ffffffff).
+-define(MSBl(Fd, Val), file:write(Fd, <<Val:32/big-unsigned-integer>>)).
+
+-define(MAXITERw, 16#ffff).
+-define(MSBw(Fd, Val), file:write(Fd, <<Val:16/big-unsigned-integer>>)).
+
+-define(MAXITERb, 16#ff).
+-define(MSBb(Fd, Val), file:write(Fd, <<Val:8/big-unsigned-integer>>)).
+
+%% -define(XMIN, -2.5).
+%% -define(XMAX, +1.5).
+%% -define(YMIN, -2.0).
+%% -define(YMAX, +2.0).
+%% -define(ZMIN, -2.0).
+%% -define(ZMAX, +2.0).
+-define(XMIN, -1.0).
+-define(XMAX, +1.0).
 -define(YMIN, -1.0).
 -define(YMAX, +1.0).
 -define(ZMIN, -1.0).
@@ -27,85 +43,91 @@
 -define(M_PI,   math:pi()).
 -define(M_PI_2, ?M_PI/2.0).
 
+-define(DEBUG, true).
 
+-ifdef(DEBUG).
+-define(CHECK(Val), ((true = (Val >= 0)) and (true = (Val =< ?MAXITERb)))).
+-else.
+-define(CHECK(Val), ok).
+-endif.
+
+-define(GEN, gen).
+
+
+s() ->
+    start().
 start() ->
     start(?FNAME).
 start(Filename) ->
     %% open file for writing
     case file:open(Filename, [write, raw, binary]) of
 	{ok, Fd} ->
-	    main(Fd),
+	    process_flag(trap_exit, true),
+	    GenPid = spawn_link(?MODULE, gen0, [self()]),
+	    receive
+		started ->
+		    ok
+	    end,
+	    main(Fd, GenPid),
 	    file:close(Fd);
 	Error ->
 	    exit(Error)
     end.
 
 
--define(MSB(Fd, Val), file:write(Fd, <<Val:16/big-unsigned-integer>>)).
+main(Fd, GenPid) ->
+    %% .df3 header, ushort each
+    ?MSBw(Fd, ?SIZE),
+    ?MSBw(Fd, ?SIZE),
+    ?MSBw(Fd, ?SIZE),
 
-main(Fd) ->
-    %% .df3 header
-    ?MSB(Fd, ?SIZE),
-    ?MSB(Fd, ?SIZE),
-    ?MSB(Fd, ?SIZE),
-
-    %% Here we goooo !
-    zloop(Fd, ?ZMAX, ?SIZE).
+    loop0(Fd, GenPid, []).
 
 
-zloop(_Fd, _Z, 0) ->
-    ok;
-zloop(Fd, Z, N) ->
-    io:format("Z= ~p~n", [N]),
-    yloop(Fd, ?YMAX, Z, ?SIZE),
-    zloop(Fd, Z-?DZ, N-1).
-
-
-yloop(_Fd, _Y, _Z, 0) ->
-    ok;
-yloop(Fd, Y, Z, N) ->
-    %% io:format("Z= ~p Y= ~p ", [LZ, N-1]),
-    Refs = xloop(?XMAX, Y, Z),
-    collect(Fd, Refs),
-    yloop(Fd, Y-?DY, Z, N-1).
-
-
-xloop(X, Y, Z) ->
-    xloop1({X, Y, Z}, [], ?SIZE).
-xloop1(_Point, Acc, 0) ->
-    Acc;
-xloop1({X, Y, Z} = Point, Acc, N) ->
-    Ref = m3d(Point),
-    xloop1({X-?DX, Y, Z}, [Ref|Acc], N-1).
-
-
-m3d(Point) ->
-    Ref = make_ref(),
-    spawn(?MODULE, m3d1, [self(), Ref, Point]),
-    Ref.
-
-
-collect(_Fd, []) ->
-    ok;
-collect(Fd, [Ref|Refs]) ->
-    receive
-	{Ref, Result} ->
-	    true = (Result >= 0),
-	    true = (Result =< ?MAXITER),
-	    ?MSB(Fd, Result),
-	    collect(Fd, Refs)
+new_process() ->
+    case point() of
+	done ->
+	    undefined;
+	Point ->
+	    spawn(?MODULE, m3d, [self(), Point])
     end.
 
 
-m3d1(From, Ref, Point) ->
+%% Here we goooo !
+loop0(Fd, GenPid, []) ->
+    Pids = [new_process() || _ <- lists:seq(1, ?CORES)],
+    loop(Fd, GenPid, Pids).
+loop(_Fd, undefined, []) ->
+    ok;
+loop(Fd, GenPid, [Pid|Pids]=Procs) ->
+    receive
+	({'EXIT', GenPid, _Reason}) ->
+	    %% io:format("~p process exited with reason: ~p~n", [?GEN, _Reason]),
+	    loop(Fd, undefined, Procs)
+    after 0 ->
+	    receive
+		{Pid, Result} ->
+		    ?MSBb(Fd, Result),
+		    case GenPid of
+			undefined ->
+			    loop(Fd, GenPid, Pids);
+			GenPid ->
+			    NewPid = new_process(),
+			    loop(Fd, GenPid, Pids++[NewPid])
+		    end
+	    end
+    end.
+
+
+m3d(Parent, Point) ->
     Iter = iter(Point),
-    From ! {Ref, Iter}.
+    Parent ! {self(), Iter}.
 
 
 iter(Point) ->
     iter(0, Point).
-iter(?MAXITER, _Point) ->
-    ?MAXITER;
+iter(?MAXITERb, _Point) ->
+    ?MAXITERb;
 iter(Iter, {X, Y, Z}) ->
     XYZ2 = X*X + Y*Y + Z*Z,
     if
@@ -148,3 +170,84 @@ ny(Radius, Yangle, Zangle) ->
     ?POW(Radius) * math:sin(Yangle*2+?M_PI_2) * math:sin(Zangle*2+?M_PI).
 nz(Radius, Yangle, _Zangle) ->
     ?POW(Radius) * math:cos(Yangle*2+?M_PI_2).
+
+
+%% 3D-points generator
+-record(gs, {i={0,0,0}, f={?XMIN, ?YMIN, ?ZMIN}}).
+
+gen0(Parent) ->
+    register(?GEN, self()),
+    Parent ! started,
+    gen(#gs{}).
+gen(State) ->
+    pp(State),
+    receive
+	tick ->
+	    stats(State),
+	    gen(State)
+    after 0 ->
+	    receive
+		{Pid, Ref, get} ->
+		    {Point, NewState} = getx(State),
+		    Pid ! {Ref, Point},
+		    case NewState of
+			done ->
+			    done;
+			NewState ->
+			    gen(NewState)
+		    end
+	    end
+    end.
+
+
+stats(_State) ->
+    'TODO'.
+
+
+point() ->
+    Ref = make_ref(),
+    ?GEN ! {self(), Ref, get},
+    receive
+	{Ref, Point} ->
+	    Point
+    end.
+
+
+%% loop on x, y and z
+getx(#gs{i={XI, YI, ZI}, f={XF, YF, ZF}=Point} = State) ->
+    %% First, try to increment XI
+    XIp1 = XI+1,
+    if
+	XIp1 == ?SIZE -> %% X done
+	    gety(State#gs{i={0, YI, ZI}, f={?XMIN, YF, ZF}});
+	true ->
+	    {Point, State#gs{i={XIp1, YI, ZI}, f={XF+?DX, YF, ZF}}}
+    end.
+
+gety(#gs{i={_XI, YI, ZI}, f={XF, YF, ZF}=Point} = State) ->
+    %% Then, try to increment YI
+    YIp1 = YI+1,
+    if
+	YIp1 == ?SIZE -> %% Y done
+	    getz(State#gs{i={0, 0, ZI}, f={?XMIN, ?YMIN, ZF}});
+	true ->
+	    {Point, State#gs{i={0, YIp1, ZI}, f={XF, YF+?DY, ZF}}}
+    end.
+
+getz(#gs{i={_XI, _YI, ZI}, f={XF, YF, ZF}=Point} = State) ->
+    %% Finally, try to increment ZI
+    ZIp1 = ZI+1,
+    if
+	ZIp1 == ?SIZE -> %% Z done
+	    {Point, done};
+	true ->
+	    {Point, State#gs{i={0, 0, ZIp1}, f={XF, YF, ZF+?DZ}}}
+    end.
+
+-ifdef(DEBUG).
+pp(#gs{i=Coord, f=Point}) ->
+    io:format("~p: ~p~n", [Coord, Point]).
+-else.
+pp(_) ->
+    ok.
+-endif.
